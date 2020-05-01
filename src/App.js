0e5d6f21af2cf3cef8c1153b0a05695ebaf538d7
Bryan Ramos <ramos.bryan100@gmail.com>
2020-05-01 10:18:01 -0600

    init project, installed dependencies

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom';
+
+
+export default function App() {
+    return (
+        <React.Fragment>
+            <span style={{display: "none"}}>Created by Bryan Ramos</span>
+        </React.Fragment>
+    );
+}
